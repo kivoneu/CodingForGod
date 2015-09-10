2aa2ce108fd8c6255f37c648183469dba24093ce
mnowik <mnowik@gmail.com>
2015-09-10 18:02:51 +0200

    tutorial.03.04

--- wsgi/myproject/polls/views.py
+++ wsgi/myproject/polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([p.question_text for p in latest_question_list])
