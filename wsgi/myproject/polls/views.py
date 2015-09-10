ff8ca309bea0db7e840218f9c4d95dd7230bfba4
mnowik <mnowik@gmail.com>
2015-09-10 17:58:57 +0200

    tutorial.03.03

--- wsgi/myproject/polls/views.py
+++ wsgi/myproject/polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
